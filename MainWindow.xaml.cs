3d05bf7b026f965be3fe775e411f55bb6b1359de
Flafio <81521675+Mausina@users.noreply.github.com>
2026-05-21 14:33:23 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using GymApp.Data;
+using GymApp.Models;
+
+namespace GymApp
+{
+    public partial class MainWindow : Window
+    {
+        ClubData db = new ClubData();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Q 1(i) Display Members
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadMembers();
+        }
+
+        private void LoadMembers()
+        {
+
+            var query = db.Members.OrderBy(m => m.Surname).ToList();
+            lbxMembers.ItemsSource = query;
+        }
+
+        // Q 1(j) Selection Changed
+        private void lbxMembers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbxMembers.SelectedItem is Member selectedMember)
+            {
+
+                txtId.Text = $"ID: {selectedMember.MemberId}";
+                txtFirstName.Text = $"First Name: {selectedMember.FirstName}";
+                txtLastName.Text = $"Last Name: {selectedMember.Surname}";
+                txtContact.Text = $"Contact Number: {selectedMember.ContactNumber}";
+                txtMembership.Text = $"Membership Type: {selectedMember.MembershipType}";
+                txtDOB.Text = $"DOB: {selectedMember.DateOfBirth:dd/MM/yyyy}";
+
+                LoadSessionsForMember(selectedMember.MemberId);
+            }
+        }
+
+        public void LoadSessionsForMember(int memberId)
+        {
+
+            var sessions = db.TrainingSessions
+                             .Where(ts => ts.MemberId == memberId)
+                             .OrderBy(ts => ts.SessionDate)
+                             .ToList();
+
+            if (sessions.Count == 0)
+            {
+                lbxSessions.Visibility = Visibility.Collapsed;
+                txtNoSessions.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                lbxSessions.Visibility = Visibility.Visible;
+                txtNoSessions.Visibility = Visibility.Collapsed;
+                lbxSessions.ItemsSource = sessions;
+            }
+        }
+
+        private void btnAddSession_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbxMembers.SelectedItem is Member selectedMember)
+            {
+                AddSessionWindow addWin = new AddSessionWindow(selectedMember, db);
+                addWin.Owner = this;
+                addWin.ShowDialog();
+
+
+                LoadSessionsForMember(selectedMember.MemberId);
+            }
+            else
+            {
+                MessageBox.Show("Please select a member first.");
+            }
+        }
+    }
+}
---
+++ TrainingSession.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GymApp.Models
+{
+    // Q 1(c) Member class
+    public class Member
+    {
+        public int MemberId { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ContactNumber { get; set; }
+        public string MembershipType { get; set; }
+
+        // Q 1(e) One to Many relationship 
+        public virtual ICollection<TrainingSession> TrainingSessions { get; set; }
+
+        public string DisplayName => $"{Surname}, {FirstName} - {ContactNumber}";
+    }
+
+    // Q 1(d) Training Session class
+    public class TrainingSession
+    {
+        public int SessionId { get; set; }
+        public DateTime SessionDate { get; set; }
+        public string SessionType { get; set; }
+        public int DurationMinutes { get; set; }
+        public string CoachNotes { get; set; }
+
+        public int MemberId { get; set; }
+        public virtual Member Member { get; set; }
+
+        public string DisplayNotes => CoachNotes?.Length > 50 ? CoachNotes.Substring(0, 50) + "..." : CoachNotes;
+    }
+}
